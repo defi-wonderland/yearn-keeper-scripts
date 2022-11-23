a46a4ca94b61864727ffca0e66fe00583f81da29
0xng <ng@defi.sucks>
2022-11-23 09:15:39 -0300

    fix: setting FUTURE_BLOCKS to non-zero value

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,11 +4,11 @@ export const CHAIN_ID = 1;
 // Size of our batch of bundles
 export const BURST_SIZE = 3;
 
-// Blocks into the future to send our first batch of bundles
-export const FUTURE_BLOCKS = 0;
+// Blocks into the future to send our first batch of bundles. We recommend a non zero value.
+export const FUTURE_BLOCKS = 2;
 
 // Priority fee to use
-export const PRIORITY_FEE = 2;
+export const PRIORITY_FEE = 1.5;
 
 // Flashbots RPC. Taken from https://docs.flashbots.net
 export const FLASHBOTS_RPC = 'https://relay.flashbots.net';
